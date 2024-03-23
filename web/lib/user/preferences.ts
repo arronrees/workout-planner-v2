04328ca83a638490d0436bb8426479614a0147e2
arronrees <64908445+arronrees@users.noreply.github.com>
2024-03-23 18:23:05 +0000

    Udpated profile and preference create handlers

--- web/lib/user/preferences.ts
+++ web/lib/user/preferences.ts
@@ -3,15 +3,24 @@
 import { createClient } from '@/utils/supabase/server';
 
 export async function createUserPreferences(userId: string) {
-  const supabase = createClient();
+  try {
+    const supabase = createClient();
 
-  const { error } = await supabase.from('user_preferences').insert({
-    user_id: userId,
-    weight_unit: 'kg',
-  });
+    const { error } = await supabase.from('user_preferences').insert({
+      user_id: userId,
+      weight_unit: 'kg',
+    });
 
-  return {
-    success: !error,
-    error,
-  };
+    return {
+      success: !error,
+      error,
+    };
+  } catch (err) {
+    console.log(err);
+
+    return {
+      success: false,
+      error: 'Something went wrong creating the user profile.',
+    };
+  }
 }
--- web/lib/user/profile.ts
+++ web/lib/user/profile.ts
@@ -3,15 +3,24 @@
 import { createClient } from '@/utils/supabase/server';
 
 export async function createUserProfile(userId: string, name: string) {
-  const supabase = createClient();
+  try {
+    const supabase = createClient();
 
-  const { error } = await supabase.from('user_profile').insert({
-    user_id: userId,
-    display_name: name,
-  });
+    const { error } = await supabase.from('user_profile').insert({
+      user_id: userId,
+      display_name: name,
+    });
 
-  return {
-    success: !error,
-    error,
-  };
+    return {
+      success: !error,
+      error,
+    };
+  } catch (err) {
+    console.log(err);
+
+    return {
+      success: false,
+      error: 'Something went wrong creating the user profile.',
+    };
+  }
 }
