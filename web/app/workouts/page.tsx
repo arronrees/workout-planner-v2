76accd43c0f39cce4a67d7b528bbaa59672d7074
arronrees <64908445+arronrees@users.noreply.github.com>
2024-04-03 21:42:17 +0100

    Load workouts on workouts page

--- web/app/workouts/page.tsx
+++ web/app/workouts/page.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import { ArrowUpRight } from 'lucide-react';
+import { ArrowRight, ArrowUpRight } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import {
   Card,
@@ -31,6 +31,12 @@ export default async function Workouts() {
     redirect('/');
   }
 
+  const { data: workouts, error: workoutError } = await supabase
+    .from('workouts')
+    .select('*, workout_exercises(id)')
+    .eq('user_id', user.id)
+    .limit(8);
+
   return (
     <div className='flex flex-1 flex-col gap-4 p-2 md:gap-8 md:p-6'>
       <div className='grid gap-4 md:gap-8 lg:grid-cols-2 xl:grid-cols-3'>
@@ -55,33 +61,32 @@ export default async function Workouts() {
                   <TableHead>Equipment Needed</TableHead>
                   <TableHead>Muscle Focus</TableHead>
                   <TableHead>No. of Exercises</TableHead>
+                  <TableHead></TableHead>
                 </TableRow>
               </TableHeader>
               <TableBody>
-                <TableRow>
-                  <TableCell>
-                    <div className='font-medium'>Upper body</div>
-                  </TableCell>
-                  <TableCell>Full</TableCell>
-                  <TableCell>Chest</TableCell>
-                  <TableCell>8</TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>
-                    <div className='font-medium'>Lower body</div>
-                  </TableCell>
-                  <TableCell>Full</TableCell>
-                  <TableCell>Legs</TableCell>
-                  <TableCell>6</TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>
-                    <div className='font-medium'>Full body</div>
-                  </TableCell>
-                  <TableCell>Basic</TableCell>
-                  <TableCell>Full</TableCell>
-                  <TableCell>10</TableCell>
-                </TableRow>
+                {workouts &&
+                  workouts.map((workout) => (
+                    <TableRow key={workout.id}>
+                      <TableCell>
+                        <div className='font-medium'>{workout.name}</div>
+                      </TableCell>
+                      <TableCell>{workout.equipment_needed}</TableCell>
+                      <TableCell>{workout.muscle_focus}</TableCell>
+                      <TableCell>{workout.workout_exercises.length}</TableCell>
+                      <TableCell>
+                        <Button asChild variant='outline'>
+                          <Link
+                            href={`/workouts/${workout.id}`}
+                            className='flex gap-1'
+                          >
+                            View
+                            <ArrowRight className='h-3 w-3' />
+                          </Link>
+                        </Button>
+                      </TableCell>
+                    </TableRow>
+                  ))}
               </TableBody>
             </Table>
           </CardContent>
